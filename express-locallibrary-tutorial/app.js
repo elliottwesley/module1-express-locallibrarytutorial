c98c5262e94197ce0ccc3208253441145186a871
elliottwesley <aquariuswre@ksu.edu>
2019-10-24 08:41:30 -0500

    "Last prep for deployment on a server."

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -13,7 +13,8 @@ var app = express();
 
 // set up a Mongoose connection
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb+srv://elliottwesley:FtzPzi3s7@wre-learningmongo-ht4jt.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://elliottwesley:FtzPzi3s7@wre-learningmongo-ht4jt.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {
  useNewUrlParser: true,
  useUnifiedTopology: true
